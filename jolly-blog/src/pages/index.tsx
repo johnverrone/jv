98a641e73534e21504d2db1a28d2b82b15ae09e7
John Verrone <john.verrone@gmail.com>
2021-02-18 22:10:12 -0500

    Splash on homescreen

--- jolly-blog/src/pages/index.tsx
+++ jolly-blog/src/pages/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Container from '../components/Container';
 import SEO from '../components/SEO';
+import styled from '../styles/styled';
+
+const Splash = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const IndexPage: React.FC = () => (
   <>
     <SEO title="Home" description="Software engineer located in Atlanta, GA" />
-    <Container>Gatsby site is up!</Container>
+    <Splash>Gatsby site is up!</Splash>
   </>
 );
 
