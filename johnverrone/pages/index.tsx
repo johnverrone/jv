d6b570a171f2a122a791cb5ee4e52282fc163f6f
John Verrone <john.verrone@gmail.com>
2021-09-15 21:01:22 -0600

    Added cover background'

--- johnverrone/pages/index.tsx
+++ johnverrone/pages/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Image from 'next/image';
+import styled from '@emotion/styled';
 import { SEO } from '@components/SEO';
+import image from '@images/john.jpg';
+
+const BackgroundWrapper = styled.div`
+  position: relative;
+  height: 100vh;
+  z-index: -1;
+`;
 
 const IndexPage: React.FC = () => (
   <>
     <SEO title="John Verrone" description="Personal blog" />
+    <BackgroundWrapper>
+      <Image
+        alt="john"
+        src={image}
+        layout="fill"
+        objectFit="cover"
+        objectPosition="50% 15%"
+      />
+    </BackgroundWrapper>
   </>
 );
 
