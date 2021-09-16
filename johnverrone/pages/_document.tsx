ae01b4f62b207121c75a826f70cd224150375d22
John Verrone <john.verrone@gmail.com>
2021-09-15 21:06:20 -0600

    Added favicon

--- johnverrone/pages/_document.tsx
+++ johnverrone/pages/_document.tsx
@@ -5,10 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="icon"
-            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏔</text></svg>"
-          />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
