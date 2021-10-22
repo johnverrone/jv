f62d9e2f1d5fd55021cdf32e968bbb9410df214a
John Verrone <john.verrone@gmail.com>
2021-10-22 14:31:19 -0600

    class => className

--- johnverrone/pages/coffee/index.tsx
+++ johnverrone/pages/coffee/index.tsx
@@ -7,9 +7,9 @@ import Link from 'next/link';
 const CoffeePage: React.FC = () => {
   return (
     <>
-      <SEO title="Blog" />
+      <SEO title="Coffee" />
       <AppContainer>
-        <iframe class="airtable-embed" src="https://airtable.com/embed/shrHQSvgnRIlpgXE4?backgroundColor=greenLight&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+        <iframe className="airtable-embed" src="https://airtable.com/embed/shrHQSvgnRIlpgXE4?backgroundColor=greenLight&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
       </AppContainer>
     </>
   );
