54d506cc94315e83a03bded2e1e78a726d275029
John Verrone <john.verrone@gmail.com>
2021-10-22 14:28:56 -0600

    Add airtable embed

---
+++ johnverrone/pages/coffee/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SEO } from '@components/SEO';
+import { AppContainer } from '@components/AppContainer';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+
+const CoffeePage: React.FC = () => {
+  return (
+    <>
+      <SEO title="Blog" />
+      <AppContainer>
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrHQSvgnRIlpgXE4?backgroundColor=greenLight&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+      </AppContainer>
+    </>
+  );
+};
+
+export default CoffeePage;
