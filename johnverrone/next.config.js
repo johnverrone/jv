1715874626ee9f927e7a37404d38946547efaecd
John Verrone <john.verrone@gmail.com>
2021-12-14 19:14:14 -0700

    redirect music to top tracks site (#22)

--- johnverrone/next.config.js
+++ johnverrone/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/music',
+        destination: 'http://top-tracks-2021.vercel.app/',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
+};
