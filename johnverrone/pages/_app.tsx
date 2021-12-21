3fe94fa540648f1126c3eda6ff9f3115be9c27c3
John Verrone <johnverrone@fullstory.com>
2021-12-21 14:43:54 -0700

    fullstory/browser

--- johnverrone/pages/_app.tsx
+++ johnverrone/pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import * as FullStory from '@fullstory/browser';
 import { ThemeProvider } from '@emotion/react';
 import theme from '../styles/theme';
 import { GlobalStyles } from '@components/GlobalStyles';
 import { CoffeeButton } from '@components/CoffeeButton';
 import '../styles/reset.css';
-import '../fullstory-script';
+
+FullStory.init({ orgId: 'Q23YS' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
