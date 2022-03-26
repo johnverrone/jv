2dc51ce0b9c5b71f8761f7fd3abb747f3baa7b5e
John Verrone <john.verrone@gmail.com>
2022-03-26 10:52:00 -0500

    Add Zion photos

--- johnverrone/pages/photo.tsx
+++ johnverrone/pages/photo.tsx
@@ -4,9 +4,12 @@ import { SEO } from '@components/SEO';
 import { AppContainer } from '@components/AppContainer';
 import { WheelNav } from '@components/Navigation';
 import { PhotoList } from '@components/PhotoList';
-import iguana from '@images/photos/iguana.jpg';
-import pelican from '@images/photos/pelican.jpg';
-import seaLion from '@images/photos/sea-lion.jpg';
+import iguana from '@images/photos/galapagos/iguana.jpg';
+import pelican from '@images/photos/galapagos/pelican.jpg';
+import seaLion from '@images/photos/galapagos/sea-lion.jpg';
+import tinyHome from '@images/photos/galapagos/tiny-home.jpg';
+import overlook from '@images/photos/galapagos/overlook.jpg';
+import patriarchs from '@images/photos/galapagos/patriarchs.jpg';
 import styled from '@emotion/styled';
 
 const CollectionTitle = styled.h3`
@@ -21,6 +24,24 @@ const PhotoPage: React.FC = () => {
       <SEO title="photo" />
       <WheelNav />
       <AppContainer>
+        <CollectionTitle>Zion National Park, United States</CollectionTitle>
+        <PhotoList>
+          <Image
+            src={patriarchs}
+            alt="Court of the Patriarchs in Zion National Park"
+            objectFit="cover"
+          />
+          <Image
+            src={tinyHome}
+            alt="Tiny home outside of Zion National Park"
+            objectFit="cover"
+          />
+          <Image
+            src={overlook}
+            alt="Sunrise view of Zion Canyon Overlook"
+            objectFit="cover"
+          />
+        </PhotoList>
         <CollectionTitle>Galápagos Islands, Ecuador</CollectionTitle>
         <PhotoList>
           <Image
