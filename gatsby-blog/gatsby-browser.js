0c55d6bd9431624301b6f136c491e756647a86c4
John Verrone <john.verrone@gmail.com>
2022-04-21 17:30:34 -0500

    Remove unused modules

--- gatsby-blog/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import App from './src/App';
-
-export const wrapPageElement = ({ element, props }) => {
-  return <App {...props}>{element}</App>;
-};
--- gatsby-blog/src/styles/styled.tsx
+++
@@ -1,6 +0,0 @@
-import styled, { CreateStyled } from '@emotion/styled';
-import theme from '../gatsby-plugin-theme-ui';
-
-type Theme = typeof theme;
-
-export default styled as CreateStyled<Theme>;
