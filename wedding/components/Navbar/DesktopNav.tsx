20d9d7469980b2d4288c4df5b5aaed6080bd5117
John Verrone <john.verrone@gmail.com>
2022-07-29 11:15:50 -0500

    RSVP prog

--- wedding/components/Navbar/DesktopNav.tsx
+++ wedding/components/Navbar/DesktopNav.tsx
@@ -1,12 +1,15 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import css from './DesktopNav.module.css';
 import { NavLinks } from './NavLinks';
 
 export const DesktopNav = () => {
+  const { asPath } = useRouter();
+
   return (
     <nav className={css.navbar}>
       <ul>
-        <NavLinks />
+        <NavLinks activeRoute={asPath} />
       </ul>
     </nav>
   );
