da1b5bf0a9cabc55100a4df0bfc7e23f52f6ec65
John Verrone <john.verrone@gmail.com>
2022-09-21 18:04:31 -0500

    Remove prisma

--- wedding/prisma/migrations/20220819012722_added_attendance/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Attendance" AS ENUM ('ATTENDING', 'NOT_ATTENDING', 'UNKNOWN');
-
--- AlterTable
-ALTER TABLE "Person" ADD COLUMN     "attendance" "Attendance" NOT NULL DEFAULT 'NOT_ATTENDING';
--- wedding/prisma/migrations/20220819012803_updated_attendance_default/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Person" ALTER COLUMN "attendance" SET DEFAULT 'UNKNOWN';
