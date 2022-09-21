5dc6dc132cc6448f87bd407bee2073cebbc7341e
John Verrone <john.verrone@gmail.com>
2022-09-20 23:12:28 -0500

    commenting out tRPC stuff for now

--- wedding/src/pages/_app.tsx
+++ wedding/src/pages/_app.tsx
@@ -12,33 +12,35 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   );
 };
 
-export default withTRPC<AppRouter>({
-  config({ ctx }) {
-    if (typeof window !== 'undefined') {
-      // during client requests
-      return {
-        url: '/api/trpc',
-      };
-    }
+// export default withTRPC<AppRouter>({
+//   config({ ctx }) {
+//     if (typeof window !== 'undefined') {
+//       // during client requests
+//       return {
+//         url: '/api/trpc',
+//       };
+//     }
 
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+//     /**
+//      * If you want to use SSR, you need to use the server's full URL
+//      * @link https://trpc.io/docs/ssr
+//      */
+//     const url = process.env.VERCEL_URL
+//       ? `https://${process.env.VERCEL_URL}/api/trpc`
+//       : 'http://localhost:3000/api/trpc';
 
-    return {
-      url,
-      /**
-       * @link https://react-query-v3.tanstack.com/reference/QueryClient
-       */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
-    };
-  },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
-  ssr: true,
-})(MyApp);
+//     return {
+//       url,
+//       /**
+//        * @link https://react-query-v3.tanstack.com/reference/QueryClient
+//        */
+//       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+//     };
+//   },
+//   /**
+//    * @link https://trpc.io/docs/ssr
+//    */
+//   ssr: true,
+// })(MyApp);
+
+export default MyApp;
