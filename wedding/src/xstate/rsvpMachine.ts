865b4b1e18c4bc680e2303e8bb7c64eca430ebdc
John Verrone <john.verrone@gmail.com>
2022-11-01 19:24:13 -0500

    Use vercel url in state machine (#51)

--- wedding/src/xstate/rsvpMachine.ts
+++ wedding/src/xstate/rsvpMachine.ts
@@ -4,8 +4,12 @@ import { createTRPCClient } from '@trpc/client';
 import { InferMutationInput, InferQueryInput } from '../utils/trpc';
 import { createMachine, assign } from 'xstate';
 
+const url = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+  : 'http://localhost:3000/api/trpc';
+
 const client = createTRPCClient<AppRouter>({
-  url: 'http://localhost:3000/api/trpc',
+  url,
 });
 
 type FindInvitationRequest = InferQueryInput<'invitations.find'>;
