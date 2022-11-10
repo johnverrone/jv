1d60ce72f3e1d4f79223968ac7338c767bc11e37
John Verrone <john.verrone@gmail.com>
2022-11-09 20:05:00 -0600

    WIP photos

--- wedding/src/pages/our-story.tsx
+++ wedding/src/pages/our-story.tsx
@@ -1,8 +1,30 @@
 import { SectionHeader } from '../components/SectionHeader';
 import { Text } from '../components/Text';
 import Head from 'next/head';
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import css from './our-story.module.scss';
+import Image from 'next/image';
+import * as photos from '../photos';
+import { motion } from 'framer-motion';
+
+interface StoryImageProps {
+  src: ComponentProps<typeof Image>['src'];
+  alt: string;
+  caption: string;
+}
+
+const StoryImage = ({ src, alt, caption }: StoryImageProps) => (
+  <motion.figure
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    viewport={{ margin: '-100px' }}
+  >
+    <Image className={css.image} src={src} alt={alt} />
+    <Text variant="body3" tag="figcaption">
+      {caption}
+    </Text>
+  </motion.figure>
+);
 
 export default function OurStory({}) {
   return (
@@ -32,8 +54,44 @@ export default function OurStory({}) {
         </Text>
         <Text variant="body2" tag="p">
           We could go on, but we don&apos;t want to bore you with a novel.
-          Instead, here are a few photos that tell the story of us. Please
-          enjoy.
+          Instead, let&apos; embark on a photo journey. Please enjoy.
+        </Text>
+        <Text variant="heading1">2017</Text>
+        <StoryImage
+          src={photos.storageUnit}
+          alt="Molly in the storage unit"
+          caption="Where it all started! John, Justin, and Brian help Molly pack away
+            her belongings from NYC. (She moved in with Lauren & Wes
+            temporarily)"
+        />
+        brian & lauras wedding
+        <Text variant="heading1">2018</Text>
+        <StoryImage
+          src={photos.running}
+          alt="John and Molly after a run with the Atlanta skyling in the background"
+          caption="In the early days, Molly joined in on TMRC (Tuesday Morning Run
+            Club) to spend time with the Atlanta crew. Unbeknownst to John,
+            Molly hated running but pretended to enjoy it to spend time with
+            him. 🥹"
+        />
+        <StoryImage
+          src={photos.prr}
+          alt="Selfie of John and Molly after runnnig the Peachtre Road Race"
+          caption="She even went as far as signing up and completing the 2018 Peachtree
+            Road Race, an annual 10k held in Atlanta, GA."
+        />
+        <StoryImage
+          src={photos.musicMidtown}
+          alt="Selfie of John and Molly at the 2018 Music Midtown festival"
+          caption="Fast forward past our first official date."
+        />
+        <StoryImage
+          src={photos.pinkAgenda2018}
+          alt="John and Molly at the annual Pink Agenda Gala"
+          caption=""
+        />
+        <Text variant="body2" tag="p">
+          And that brings us to today. We look forward to celebrating with you.
         </Text>
         <Text variant="body2" tag="p">
           &lt;3 jolly
