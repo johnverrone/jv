f41be3e3291970c48adb2543d02fbf87bb7fc1a2
John Verrone <johnverrone@fullstory.com>
2022-12-29 11:37:52 -0500

    Photo captions

--- wedding/src/pages/index.tsx
+++ wedding/src/pages/index.tsx
@@ -24,160 +24,173 @@ const images: JollyImage[] = [
   {
     id: 'mt-evans',
     src: heroImage,
-    caption: 'Molly and John on the summit of Mt. Evans',
+    caption: 'Summit of Mt. Evans - May 2021',
     objectFit: 'cover',
     objectPosition: '66%',
   },
   {
     id: 'storage',
     src: photos.storageUnit,
-    caption: 'Molly in the storage unit where Molly and John first met',
+    caption: 'The storage unit where Molly and John first met - August 2017',
   },
   {
     id: 'music-midtown',
     src: photos.musicMidtown,
-    caption: 'Molly and John at the 2018 Music Midtown festival',
+    caption: 'Music Midtown festival - September 2018',
   },
   {
     id: 'pink-agenda',
     src: photos.pinkAgenda2018,
-    caption: 'Molly and John at the annual Pink Agenda Gala',
+    caption: 'Annual Pink Agenda Gala in Atlanta - October 2018',
   },
   {
     id: 'arches',
     src: photos.arches,
-    caption: '',
+    caption: 'Arches National Park - November 2021',
   },
   {
     id: 'austinF1',
     src: photos.austinF1,
-    caption: '',
+    caption:
+      'Downtown Austin before the Formula 1 US Grand Prix - October 2022',
   },
   {
     id: 'canyons',
     src: photos.canyons,
-    caption: '',
+    caption: 'After a solid day on the slopes in Park City - January 2022',
   },
   {
     id: 'capitalReef',
     src: photos.capitalReef,
-    caption: '',
+    caption: 'Capital Reef National Park - November 2021',
   },
   {
     id: 'devilsKitchen',
     src: photos.devilsKitchen,
-    caption: '',
+    caption:
+      "On the Devil's Kitchen trail in Grand Junction, CO - November 2021",
   },
   {
     id: 'f1',
     src: photos.f1,
-    caption: '',
+    caption:
+      'Sunset over Circuit of the Americas after the F1 race - October 2022',
   },
   {
     id: 'florence',
     src: photos.florence,
-    caption: '',
+    caption: 'On the Ponte Vecchio in Florence, Italy - June 2022',
   },
   {
     id: 'getaway',
     src: photos.getaway,
-    caption: '',
+    caption: 'Getaway Cabins in the North Georgia Mountains - February 2020',
   },
   {
     id: 'goingAway',
     src: photos.goingAway,
-    caption: '',
+    caption:
+      'Going away party thrown by amazing friends right before we ventured off for our nomad year - May 2021',
   },
   {
     id: 'grandJunction',
     src: photos.grandJunction,
-    caption: '',
+    caption: 'Redlands Mesa Golf Club in Grand Junction, CO - October 2021',
   },
   {
     id: 'halloween',
     src: photos.halloween,
-    caption: '',
+    caption:
+      'Halloween! John was a bag of East Pole coffee beans and Molly was a Chemex - October 2021',
   },
   {
     id: 'jackies',
     src: photos.jackies,
-    caption: '',
+    caption: "At Jackie's (John's cousin) wedding in Wilmington, NC - May 2021",
   },
   {
     id: 'lakeComo',
     src: photos.lakeComo,
-    caption: '',
+    caption: 'Selfie on Lake Como - June 2022',
   },
   {
     id: 'mirrors',
     src: photos.mirrors,
-    caption: '',
+    caption:
+      "In Yayoi Kusama's Infinity Mirror Rooms exhibit in Atlanta - January 2019",
     objectFit: 'cover',
   },
   {
     id: 'moving',
     src: photos.moving,
-    caption: '',
-  },
-  {
-    id: 'proposal',
-    src: photos.proposal,
-    caption: '',
+    caption: 'Packed up and ready to drive across country - May 2021',
   },
   {
     id: 'proposalKnee',
     src: photos.proposalKnee,
-    caption: '',
+    caption: 'John popping the question in Brooklyn, NY - June 2022',
+  },
+  {
+    id: 'proposal',
+    src: photos.proposal,
+    caption: 'She said yes! June 2022',
   },
   {
     id: 'scottsdaleGolf',
     src: photos.scottsdaleGolf,
-    caption: '',
+    caption: "Golfing in Scottsdale, AZ for Molly's 30th - January 2021",
   },
   {
     id: 'shivanas',
     src: photos.shivanas,
-    caption: '',
+    caption:
+      "Dancing at JJ and Shivana's wedding in Asheville, NC - September 2021",
   },
   {
     id: 'slcHike',
     src: photos.slcHike,
-    caption: '',
+    caption:
+      'Summit of Grandeur Peak just outside of Salt Lake City, UT - January 2022',
   },
   {
     id: 'snowbasin',
     src: photos.snowbasin,
-    caption: '',
+    caption: "Top of Allen's Peak in Snowbasin - January 2022",
   },
   {
     id: 'steamboat',
     src: photos.steamboat,
-    caption: '',
+    caption:
+      'Hiking to Rabbit Ears Peak near Steamboat Springs, CO - July 2021',
   },
   {
     id: 'telluride',
     src: photos.telluride,
-    caption: '',
+    caption:
+      'Hiking the Bridal Veil Falls trail in Telluride, CO - November, 2021',
   },
   {
     id: 'theWorksFam',
     src: photos.theWorksFam,
-    caption: '',
+    caption:
+      "Grabbing pre dinner drinks with John's family at The Works in Atlanta - December 2021",
   },
   {
     id: 'theWorks',
     src: photos.theWorks,
-    caption: '',
+    caption: 'All smiles for Bacchanalia - December 2021',
   },
   {
     id: 'vaFam',
     src: photos.vaFam,
-    caption: '',
+    caption:
+      "Celebrating Kyle's (Molly's brother) graduation from Darden in Charlottesville, VA - May 2022",
   },
   {
     id: 'zilker',
     src: photos.zilker,
-    caption: '',
+    caption:
+      'A beautiful day in Zilker Park after Molly & John finally settled in Austin, TX - May 2022',
   },
 ];
 
