c7c544258ff4a57f2e4d8d631e9a279f7b409df7
John Verrone <john.verrone@gmail.com>
2023-06-08 20:33:46 -0600

    Revamp photos page and move images to supabase bucket

--- johnverrone/next.config.js
+++ johnverrone/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wnnjdgkrwtehvvxfqohk.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/portfolio/**',
+      },
+    ],
     domains: [
       's3.us-west-2.amazonaws.com',
       'dl.airtable.com',
