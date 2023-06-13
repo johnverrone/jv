5dd609af6e092701f70cef238c71e8cbd829857b
John Verrone <john.verrone@gmail.com>
2023-06-12 21:23:51 -0600

    [johnverrone] Use Supabase Client to load images (#93)

--- johnverrone/app/photo/page.tsx
+++ johnverrone/app/photo/page.tsx
@@ -1,10 +1,33 @@
 import { Metadata } from 'next';
+import supabase from '../../lib/supabase';
 import PhotoPage from './photo-page';
+
+async function getAllImages() {
+  const { data, error } = await supabase.storage
+    .from('portfolio')
+    .list(undefined, {
+      sortBy: { column: 'name', order: 'desc' },
+    });
+
+  if (error) {
+    throw new Error('failed to fetch images from supabase');
+  }
+
+  return (
+    data?.map((image) => {
+      const { data: imageUrl } = supabase.storage
+        .from('portfolio')
+        .getPublicUrl(image.name);
+      return imageUrl.publicUrl;
+    }) ?? []
+  );
+}
 
 export const metadata: Metadata = {
   title: 'photo',
 };
 
-export default function Page() {
-  return <PhotoPage />;
+export default async function Page() {
+  const images = await getAllImages();
+  return <PhotoPage images={images} />;
 }
---
+++ johnverrone/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+export default createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
+);
