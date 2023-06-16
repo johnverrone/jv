3f13afcbc317c6523be81a8f736f1307d675d999
John Verrone <john.verrone@gmail.com>
2023-06-15 18:46:44 -0600

    [wedding] Update API URL (#95)

--- wedding/src/utils/trpc.ts
+++ wedding/src/utils/trpc.ts
@@ -1,9 +1,8 @@
-import { createTRPCProxyClient } from '@trpc/client';
-import { httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import { AppRouter } from '../server/routers/_app';
 
 const url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+  ? `https://www.johnmolly.com/api/trpc`
   : 'http://localhost:3000/api/trpc';
 
 export const trpcClient = createTRPCProxyClient<AppRouter>({
