8cd3b2b5292a85529119f321799027aa18e94a77
John Verrone <john.verrone@gmail.com>
2023-10-24 20:25:38 -0600

    [johnverrone] invalidates cache for photos after one hour (#111)

--- johnverrone/app/photo/page.tsx
+++ johnverrone/app/photo/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next';
 import supabase from '../../lib/supabase';
 import PhotoPage from './photo-page';
+import { cache } from 'react';
+
+export const revalidate = 3600; // revalidate the data at most every hour
 
 async function getAllImages() {
   const { data, error } = await supabase.storage
@@ -23,11 +26,13 @@ async function getAllImages() {
   );
 }
 
+const cachedGetAllImages = cache(getAllImages);
+
 export const metadata: Metadata = {
   title: 'photo',
 };
 
 export default async function Page() {
-  const images = await getAllImages();
+  const images = await cachedGetAllImages();
   return <PhotoPage images={images} />;
 }
