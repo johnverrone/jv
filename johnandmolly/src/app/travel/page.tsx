337a9e658a5c97cb53640becdb5fd1023696a5f0
John Verrone <john.verrone@gmail.com>
2023-11-08 20:26:34 -0700

    responsive design woo

--- johnandmolly/src/app/travel/page.tsx
+++ johnandmolly/src/app/travel/page.tsx
@@ -16,7 +16,7 @@ export default function TravelPage() {
     <div className="flex flex-col gap-8">
       {locations.map(({ date, img, name, slug }) => (
         <Link href={`travel/${slug}`} key={slug}>
-          <div className="flex flex-wrap gap-2 h-60">
+          <div className="flex flex-col items-center gap-2 sm:flex-row">
             <div className=" bg-slate-200 rounded-2xl w-60 h-60">
               <img
                 src={img}
@@ -26,9 +26,9 @@ export default function TravelPage() {
                 style={{ borderRadius: 'inherit' }}
               />
             </div>
-            <div className="flex flex-col justify-center">
-              <h1 className="text-[6vw] font-bold">{name}</h1>
-              <h4 className="text-[max(1.3vw,1rem)] pl-3">{date}</h4>
+            <div className="flex flex-col items-center sm:items-start">
+              <h1 className="text-[max(6vw,2rem)] font-bold">{name}</h1>
+              <h4 className="text-[max(1.3vw,1rem)] sm:pl-4">{date}</h4>
             </div>
           </div>
         </Link>
