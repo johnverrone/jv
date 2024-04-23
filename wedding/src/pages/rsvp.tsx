cf03d004c3d3dd05a64a1f861a22f3bf40e8b50f
John Verrone <john.verrone@gmail.com>
2024-04-22 17:20:53 -1000

    shut down RSVP (#124)

--- wedding/src/pages/rsvp.tsx
+++ wedding/src/pages/rsvp.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import React from 'react';
-import { Text } from '../components/Text';
-import { RSVPForm } from '../components/RSVPForm';
-import css from './rsvp.module.scss';
+import Head from "next/head";
+import React from "react";
+import { Text } from "../components/Text";
+import { RSVPForm } from "../components/RSVPForm";
+import css from "./rsvp.module.scss";
 
 export default function Rsvp({}) {
-  const rsvpEnabled = process.env.NEXT_PUBLIC_RSVP_DISABLED !== '1';
+  const rsvpEnabled = process.env.NEXT_PUBLIC_RSVP_DISABLED !== "1";
 
   return (
     <>
@@ -23,10 +23,7 @@ export default function Rsvp({}) {
         ) : (
           <div className={css.comingSoon}>
             <Text variant="body1" tag="p">
-              We look forward to hearing from you!
-            </Text>
-            <Text variant="body3" tag="p">
-              This page will become active once invitiations have been sent out.
+              Thank you for making our day so special!
             </Text>
           </div>
         )}
