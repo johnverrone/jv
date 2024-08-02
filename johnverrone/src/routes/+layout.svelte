02bad850996bcccecd9eb4fa2afa3fc223d5cf52
John Verrone <john.verrone@gmail.com>
2024-08-02 17:20:40 -0600

    add view transitions

--- johnverrone/src/routes/+layout.svelte
+++ johnverrone/src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import './reset.css';
 	import './globals.css';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <slot />
