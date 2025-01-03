1cdc6b3f383f2c18c368211db1b22b06b20cbe79
John Verrone <john.verrone@gmail.com>
2025-01-03 14:59:28 -0700

    revert coffee sanity stuff

--- johnverrone/eslint.config.js
+++ johnverrone/eslint.config.js
@@ -1,5 +1,4 @@
-/** @type { import("eslint").Linter.Config } */
-module.exports = {
+export default {
 	root: true,
 	extends: [
 		'eslint:recommended',
--- johnverrone/src/lib/queries.ts
+++
@@ -1,5 +0,0 @@
-export const coffeeQuery = `*[_type == "coffee" && slug.current == $slug][0]`;
-
-export interface CoffeeResult {
-	name: string;
-}
