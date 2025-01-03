c22df0ffde290021af1179c0633fb95769a8e415
John Verrone <john.verrone@gmail.com>
2025-01-03 14:32:25 -0700

    studio host

--- johnverrone/studio/sanity.cli.ts
+++ johnverrone/studio/sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'h7b2c4g0',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'johnverrone',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
