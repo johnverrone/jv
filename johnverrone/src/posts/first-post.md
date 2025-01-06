84f9dbbb1ff057001149ec8aef5eb410632724e4
John Verrone <john.verrone@gmail.com>
2025-01-05 19:16:46 -0700

    mdsvex blog

---
+++ johnverrone/src/posts/first-post.md
@@ -0,0 +1,20 @@
+---
+title: "First Post"
+description: "This is my first post!"
+date: "2024-09-24"
+image: /first-post-banner.webp
+categories:
+  - blog
+  - tutorial
+published: true
+---
+
+## Contents
+
+## Intro
+
+This is my first post!
+
+## Let's get started
+
+Lorem ipsum dolor sit amet...
---
+++ johnverrone/src/routes/(pages)/blog/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { formatDate } from '$lib/utils';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<section class="mb-16">
+	<ul class="flex flex-col items-center">
+		{#each data.posts as post}
+			<li class="post">
+				<a href={`/blog/${post.slug}`} class="title">{post.title}</a>
+				<p class="date">{formatDate(post.date)}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
