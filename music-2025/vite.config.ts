a31015180a50361e5f8e94225a043c7a51ba43d6
John Verrone <john.verrone@gmail.com>
2025-12-13 08:46:56 -0700

    remove separate app

--- music-2025/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
-});
