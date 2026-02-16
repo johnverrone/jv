0e4ae02deb8db50ef278b93cdfa1668002af699d
John Verrone <john.verrone@gmail.com>
2026-02-15 18:49:09 -0700

    `/guitar` page  (#151)

---
+++ johnverrone/src/routes/(pages)/guitar/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import SvelteMarked from 'svelte-marked';
+
+	let { data } = $props();
+</script>
+
+<article>
+	<section class="prose">
+		<SvelteMarked source={data.progress} />
+	</section>
+
+	<hr />
+
+	<section class="prose">
+		<SvelteMarked source={data.plan} />
+	</section>
+</article>
+
+<style>
+	article {
+		max-width: min(90vw, 500px);
+		margin: 0 auto;
+	}
+
+	hr {
+		margin: 3rem 0;
+	}
+</style>
